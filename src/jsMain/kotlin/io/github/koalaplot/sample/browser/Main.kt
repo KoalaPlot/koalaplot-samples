dc6f0a6f6bb6db47c1439cc168eae3010bc363e6
Greg Steckman <27028452+gsteckman@users.noreply.github.com>
2022-08-09 20:53:52 -0700

    Add resizable window for web sample. Thanks to "Oliver O"!

--- src/jsMain/kotlin/io/github/koalaplot/sample/browser/Main.kt
+++ src/jsMain/kotlin/io/github/koalaplot/sample/browser/Main.kt
@@ -1,12 +1,11 @@
 package io.github.koalaplot.sample.browser
 
-import androidx.compose.ui.window.Window
 import io.github.koalaplot.sample.MainView
 import org.jetbrains.skiko.wasm.onWasmReady
 
 fun main() {
     onWasmReady {
-        Window("Window Title") {
+        browserViewportWindow("Koala Plot Samples") {
             MainView()
         }
     }
