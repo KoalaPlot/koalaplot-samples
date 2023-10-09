b28362d3d04b1b238623f1add41033c62c9ec960
Greg Steckman <27028452+gsteckman@users.noreply.github.com>
2023-10-08 21:08:31 -0700

    Add Waterfall sample.

---
+++ src/commonMain/kotlin/io/github/koalaplot/sample/WaterfallChart.kt
@@ -0,0 +1,140 @@
+package io.github.koalaplot.sample
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.unit.dp
+import io.github.koalaplot.core.ChartLayout
+import io.github.koalaplot.core.bar.BarChartEntry
+import io.github.koalaplot.core.bar.DefaultVerticalBar
+import io.github.koalaplot.core.bar.VerticalBarChart
+import io.github.koalaplot.core.util.ExperimentalKoalaPlotApi
+import io.github.koalaplot.core.util.toString
+import io.github.koalaplot.core.xychart.CategoryAxisModel
+import io.github.koalaplot.core.xychart.LinearAxisModel
+import io.github.koalaplot.core.xychart.XYChart
+import kotlin.math.ceil
+import kotlin.math.max
+import kotlin.math.min
+
+private val BOROUGH = PopulationData.Categories.Manhattan
+
+private fun barChartEntries(): List<List<BarChartEntry<Int, Float>>> {
+    return buildList {
+        var last = 0f
+        add(
+            PopulationData.data[BOROUGH]!!.mapIndexed { index, population ->
+                val entry = if (index == PopulationData.data[BOROUGH]!!.lastIndex) {
+                    object : BarChartEntry<Int, Float> {
+                        override val xValue = PopulationData.years[index]
+                        override val yMin = 0f
+                        override val yMax = population.toFloat()
+                    }
+                } else {
+                    object : BarChartEntry<Int, Float> {
+                        override val xValue = PopulationData.years[index]
+                        override val yMin = min(last, population.toFloat())
+                        override val yMax = max(last, population.toFloat())
+                    }
+                }
+
+                last = population.toFloat()
+                entry
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalKoalaPlotApi::class)
+val waterfallChartSampleView = object : SampleView {
+    override val name: String = "Waterfall Chart"
+
+    override val thumbnail = @Composable {
+        ThumbnailTheme {
+            ChartLayout(title = { Text(name) }) {
+                WaterfallChart(true)
+            }
+        }
+    }
+
+    override val content: @Composable () -> Unit = @Composable {
+        Column {
+            ChartLayout(
+                title = {
+                    Text(
+                        "Population Changes in Manhattan Over 70 Years",
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+                },
+                modifier = Modifier.sizeIn(minHeight = 200.dp, maxHeight = 600.dp).weight(1f)
+            ) {
+                WaterfallChart(false)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalKoalaPlotApi::class)
+@Composable
+private fun WaterfallChart(thumbnail: Boolean) {
+    val barChartEntries = remember { barChartEntries() }
+
+    @Suppress("MagicNumber")
+    val p = ceil(PopulationData.data[BOROUGH]!!.last() / 1E6f) * 1E6f
+
+    XYChart(
+        xAxisModel = CategoryAxisModel(PopulationData.years),
+        yAxisModel = LinearAxisModel(0f..p),
+        xAxisTitle = "Year",
+        yAxisTitle = "Population",
+        xAxisLabels = {
+            if (!thumbnail) {
+                it.toString()
+            } else {
+                ""
+            }
+        },
+        yAxisLabels = {
+            if (!thumbnail) {
+                it.toString(0)
+            } else {
+                ""
+            }
+        },
+        verticalMajorGridLineStyle = null,
+        verticalMinorGridLineStyle = null,
+        horizontalMinorGridLineStyle = null,
+        horizontalMajorGridLineStyle = null,
+    ) {
+        @Suppress("MagicNumber")
+        VerticalBarChart(
+            series = barChartEntries,
+            bar = { _, index, _ ->
+                val color = when {
+                    index == 0 -> SolidColor(Color(0xFF00498F))
+                    index == PopulationData.data[BOROUGH]!!.lastIndex -> SolidColor(Color(0xFF00498F))
+                    PopulationData.data[BOROUGH]!![index] > PopulationData.data[BOROUGH]!![index - 1] -> SolidColor(
+                        Color(0xFF37A78F)
+                    )
+
+                    PopulationData.data[BOROUGH]!![index] < PopulationData.data[BOROUGH]!![index - 1] -> SolidColor(
+                        Color(0xFFED7D31)
+                    )
+
+                    else -> SolidColor(Color.Black) // Shouldn't happen
+                }
+                DefaultVerticalBar(
+                    brush = color,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        )
+    }
+}
