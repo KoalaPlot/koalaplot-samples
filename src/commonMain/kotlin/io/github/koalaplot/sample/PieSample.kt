416455729e0b29ed1160c080f32fe31b532217ed
Greg Steckman <27028452+gsteckman@users.noreply.github.com>
2025-09-06 14:29:14 -0700

    Remove unused import.

--- src/commonMain/kotlin/io/github/koalaplot/sample/PieSample.kt
+++ src/commonMain/kotlin/io/github/koalaplot/sample/PieSample.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
